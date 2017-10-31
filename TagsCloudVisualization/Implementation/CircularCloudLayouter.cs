cfd98c3c6e7385fd1647aab5476283a94de12755
AntonMikheev666 <AntonMikheev666@gmail.com>
2017-10-31 15:59:06 +0500

    First implementation(devil advocate)

---
+++ TagsCloudVisualization/Implementation/CircularCloudLayouter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization.Implementation
+{
+    public class CircularCloudLayouter
+    {
+        public Point Center { get; }
+
+        public CircularCloudLayouter(Point center)
+        {
+            Center = center;
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            return new Rectangle(new Point(450, 450), rectangleSize);
+        }
+    }
+}
---
+++ TagsCloudVisualization/Tests/LayouterTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TagsCloudVisualization.Implementation;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization.Tests
+{
+    [TestFixture]
+    class LayouterTests
+    {
+        private CircularCloudLayouter layouter;
+
+        [SetUp]
+        public void Setup()
+        {
+            layouter = new CircularCloudLayouter(new Point(500, 500));
+        }
+
+        [TestCase(100, 100, TestName = "Square")]
+        public void CircularCloudLayouter_FirstRectngle_Centered(int weight, int height)
+        {
+            layouter.PutNextRectangle(new Size(weight, height)).Location
+                .ShouldBeEquivalentTo(new Rectangle(new Point(450, 450), new Size(weight, height)),
+                                      opt => opt.ComparingEnumsByValue());
+        }
+    }
+}
